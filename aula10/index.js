eaf9380a19698485db18cd4c1bb07eb2e214258b
Douglas Correa Gomes <douglascgom@gmail.com>
2022-06-28 15:47:35 -0300

    Estudo JS

---
+++ aula10/index.js
@@ -0,0 +1,19 @@
+/* Operadores Aritmeticos 
+()
+**
+* / %
++-
+*/
+
+const idade = 30;
+
+let anos = 0;
+
+anos++
+console.log(idade + anos);
+
+anos++
+console.log(idade + anos);
+anos++
+console.log(idade + anos);
+
---
+++ aula9/index.js
@@ -0,0 +1,8 @@
+const nome = 'Douglas'
+
+console.log(nome)
+
+let primeiroNome = 'Correa'
+primeiroNome = 'Gomes'
+
+console.log(nome,primeiroNome)
