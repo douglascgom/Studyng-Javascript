8added88876183f030aa5b5a448d4d78ef933f47
Douglas Correa Gomes <douglascgom@gmail.com>
2022-06-28 22:02:22 -0300

    Estudos sobre variaveis

---
+++ aula11/assets/script.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt('Digite um numero'));
+let num2 = Number(prompt('Digite outro numero'));
+
+let resultado = num1 + num2 ;
+
+alert(`A soma de ${num1} + ${num2} e igual a ${resultado}`)
---
+++ aula12/script.js
@@ -0,0 +1,11 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+let primeiro = varA
+
+varA = varB
+varB = varC
+varC = primeiro
+
+console.log(varA, varB, varC)
