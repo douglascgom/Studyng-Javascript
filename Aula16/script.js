c8d5f990124ab16bb41be3cff05bf7a60a9b88d3
Douglas Correa Gomes <douglascgom@gmail.com>
2022-07-04 16:42:06 -0300

    Estudo de Funcoes

--- Aula16/script.js
+++ Aula16/script.js
@@ -1,1 +1,33 @@
+const alunos = ['Maria','Joao','Lucas'];
 
+console.log(typeof alunos);
+
+//console.log(alunos.slice(0,-2));
+
+
+//console.log(alunos[10]);
+
+//delete alunos[1]
+//console.log(alunos);
+
+//const removido = alunos.shift(); //shift = remove no comeco 
+//const removido = alunos.pop(); // pop = remove no fim
+//console.log(removido)
+//console.log(alunos)
+
+//alunos.unshift('Otavio')  // Adiciona no comeco do ARRAY
+//alunos.unshift('Fabio')
+
+//alunos.push('Rogerio'); //Adiciona no fim do ARRAY
+//alunos.push('Fabio');
+
+//alunos[alunos.length] = 'Luiza' //ADD PELO TAMANHO DO ARRAY NO FIM
+//alunos[alunos.length] = 'Fabio'
+//alunos[alunos.length] = 'Luana'
+
+//alunos[0] = 'Douglas'; // Altera os valores do array
+//alunos[3] = 'Camila'; //Adicionando valor no array
+
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]); 
---
+++ aula17/script.js
@@ -0,0 +1,34 @@
+const raiz = n => n ** 0.5;
+
+
+console.log(raiz(25));
+console.log(raiz(9));
+console.log(raiz(16));
+
+
+
+
+
+/*
+const raiz = function(n) {
+    return n ** 0.5;
+};
+
+console.log(raiz(25));
+function soma (x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma(5, 10);
+console.log(resultado);
+console.log(soma(5, 10));
+console.log(soma(2, 6));
+
+function saudacao(nome) {
+    return `Bom dia ${nome}`
+}
+
+const variavel = saudacao('Luiz')
+console.log(variavel)
+*/
